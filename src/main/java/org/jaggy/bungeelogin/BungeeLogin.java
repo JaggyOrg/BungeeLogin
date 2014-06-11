0aac8ebf1f3ad54abc31bc82e9c37d7e9ac52507
dwdcweb <dwdcweb ([at]) gmail.com>
2014-06-11 01:33:35 -0400

    did  more changes and create the class and plugin,yml

---
+++ src/main/java/org/jaggy/bungeelogin/BungeeLogin.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jaggy.bungeelogin;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ *
+ * @author Matthew
+ */
+public class BungeeLogin {
+    
+}
